0942eda0172c8d8fa80708800bfd06fdabc5ebd6
Branden Youngs <hello@brandenbuilds.com>
2021-12-28 16:21:28 -0500

    tailwind install

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: ["./src/**/*.html", "./src/**/*.css", "./src/**/*.js"],
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {},
+  },
+  plugins: [],
+};
