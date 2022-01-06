13b2fe99c1b1688ba035f300d4e70ad6cd938b4c
Branden Youngs <hello@brandenbuilds.com>
2022-01-06 09:40:39 -0500

    added browser check to fix document issues when using node server

--- src/utils/visibility.js
+++ src/utils/visibility.js
@@ -1,24 +1,32 @@
+import { isBrowser } from "./isBroswer";
+
 export function getBrowserVisibilityProp() {
-  if (typeof document.hidden !== "undefined") {
-    // Opera 12.10 and Firefox 18 and later support
-    return "visibilitychange";
-  } else if (typeof document.msHidden !== "undefined") {
-    return "msvisibilitychange";
-  } else if (typeof document.webkitHidden !== "undefined") {
-    return "webkitvisibilitychange";
+  if (isBrowser()) {
+    if (typeof document.hidden !== "undefined") {
+      // Opera 12.10 and Firefox 18 and later support
+      return "visibilitychange";
+    } else if (typeof document.msHidden !== "undefined") {
+      return "msvisibilitychange";
+    } else if (typeof document.webkitHidden !== "undefined") {
+      return "webkitvisibilitychange";
+    }
   }
 }
 
 export function getBrowserDocumentHiddenProp() {
-  if (typeof document.hidden !== "undefined") {
-    return "hidden";
-  } else if (typeof document.msHidden !== "undefined") {
-    return "msHidden";
-  } else if (typeof document.webkitHidden !== "undefined") {
-    return "webkitHidden";
+  if (isBrowser()) {
+    if (typeof document.hidden !== "undefined") {
+      return "hidden";
+    } else if (typeof document.msHidden !== "undefined") {
+      return "msHidden";
+    } else if (typeof document.webkitHidden !== "undefined") {
+      return "webkitHidden";
+    }
   }
 }
 
 export function getIsDocumentHidden() {
-  return !document[getBrowserDocumentHiddenProp()];
+  if (isBrowser()) {
+    return !document[getBrowserDocumentHiddenProp()];
+  }
 }
